dbe803a915b6b90c1126a5cd0aa23c4dae520a53
SyllableSpec <59154499+SyllableSpec@users.noreply.github.com>
2022-06-08 23:07:24 +0800

    Update Network-Info.js

--- JS/Network-Info.js
+++ JS/Network-Info.js
@@ -57,7 +57,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
       title: wifi.ssid ? wifi.ssid : cellularInfo,
       content:
         (v4.primaryAddress ? `设备IP：${v4.primaryAddress} \n` : '') +
-        (v6.primaryAddress ? `IPv6地址已分配\n` : '') +
+        (v6.primaryAddress ? `已分配IPv6地址\n` : '') +
         (v4.primaryRouter && wifi.ssid ? `路由器IP：${v4.primaryRouter}\n` : '') +
         (v6.primaryRouter && wifi.ssid ? `IPv6地址已分配\n` : '') +
         `节点IP：${info.query}\n` +
