0a6dedeb56937409af99b5cbdd1c353c648bcf49
SyllableSpec <59154499+SyllableSpec@users.noreply.github.com>
2022-06-08 19:03:48 +0800

    Update Network-Info.js

--- JS/Network-Info.js
+++ JS/Network-Info.js
@@ -58,8 +58,8 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
       content:
         (v4.primaryAddress ? `设备IPv4：${v4.primaryAddress} \n` : '') +
         (v6.primaryAddress ? `设备IPv6：${v6.primaryAddress}\n` : '') +
-        (v4.primaryRouter && wifi.ssid ? `Router IP：${v4.primaryRouter}\n` : '') +
-        (v6.primaryRouter && wifi.ssid ? `Router IPv6：${v6.primaryRouter}\n` : '') +
+        (v4.primaryRouter && wifi.ssid ? `Router IPv4：${v4.primaryRouter}\n` : '') +
+        (v6.primaryRouter && wifi.ssid ? `Router IPv6：{v6.primaryRouter}\n` : '') +
         `节点IP：${info.query}\n` +
         `节点ISP：${info.isp}\n` +
         `节点位置：${getFlagEmoji(info.countryCode)} | ${info.country} - ${info.city
