4452fd64a0a1fee04da0d5b80194e0f240a480f4
Rabbit-Spec <59154499+Rabbit-Spec@users.noreply.github.com>
2022-06-15 10:53:59 +0800

    Update Stream-All.js

--- JS/Stream-All.js
+++ JS/Stream-All.js
@@ -1,3 +1,6 @@
+/**
+* 感谢由@LucaLin233编写的js，原脚本地址：https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/stream-all.js
+
 const REQUEST_HEADERS = {
     'User-Agent':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
