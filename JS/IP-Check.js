9f5e51cee5a31fb8084f6645870975f7e0081d41
Rabbit-Spec <59154499+Rabbit-Spec@users.noreply.github.com>
2022-06-15 10:52:02 +0800

    Update IP-Check.js

--- JS/IP-Check.js
+++ JS/IP-Check.js
@@ -1,3 +1,6 @@
+/**
+* 感谢由@congcong大佬编写的js，原脚本地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
+
 let url = "http://ip-api.com/json"
 
 $httpClient.get(url, function(error, response, data){
