e3abc37637148c750663626e947062ac43349572
Rabbit-Spec <59154499+Rabbit-Spec@users.noreply.github.com>
2022-06-16 00:48:03 +0800

    Update Group-Panel.js

--- Panel/Group-Panel/Group-Panel.js
+++ Panel/Group-Panel/Group-Panel.js
@@ -52,7 +52,7 @@ let secondName;
 let rootName = name;
 if(allGroup.includes(rootName)==true){
 	secondName = (await httpAPI("/v1/policy_groups/select?group_name="+encodeURIComponent(rootName)+"")).policy;
-	name = name + '\n' + secondName
+	name = 策略：name + '\n' + 节点：secondName
 }
 
 while(allGroup.includes(rootName)==true){
@@ -60,7 +60,7 @@ while(allGroup.includes(rootName)==true){
 }
 
 if(arr[index].isGroup==true && secondName!= rootName){
-name=name + '\n' + rootName;
+name=策略：name + '\n' + 节点：rootName;
 }
 
     $done({
